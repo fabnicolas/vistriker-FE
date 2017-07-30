2ee15a9c873df0c4922ab90f775e44535bacbcae
Finalgalaxy <finalgalaxy@gmail.com>
2017-07-30 10:37:27 +0200

    Made material-content-grid component responsive.

--- src/app/material-content-grid/material-content-grid.component.ts
+++ src/app/material-content-grid/material-content-grid.component.ts
@@ -5,18 +5,32 @@ import 'rxjs/Rx';
 @Component({
   selector: 'app-material-content-grid',
   templateUrl: './material-content-grid.component.html',
-  styleUrls: ['./material-content-grid.component.css']
+  styleUrls: ['./material-content-grid.component.css'],
+  host: {'(window:resize)': 'onResize($event)'}
 })
 export class MaterialContentGridComponent implements OnInit {
   arr_videos: Array<any>;
 
+  numcols: number = 6;
+
   constructor(private http: Http){
-    this.http.get('http://localhost:8200/get_sample_videos')
+    this.http.get('http://localhost:8200/get_example_videos')
       .map(response => response.json())
       .subscribe(res => this.arr_videos = res);
+    this.handleResponsiveLayout(window.screen.width);
   }
 
   ngOnInit() {
+  }
+
+  onResize(event) {
+    this.handleResponsiveLayout(event.target.innerWidth);
+  }
+
+  handleResponsiveLayout(width){
+    if(width < 750)      this.numcols=2;
+    else if(width < 950) this.numcols=4;
+    else                 this.numcols=6;
   }
 
 }
